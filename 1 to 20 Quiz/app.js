aa5b05453ee7f0b6032484679b21039afa77dc91
Iqran09 <133879912+Iqran09@users.noreply.github.com>
2023-05-17 18:50:39 +0500

    Add files via upload

---
+++ 1 to 20 Quiz/app.js
@@ -0,0 +1,225 @@
+// QUIZ
+// =============================================================================================
+
+// CHAPTER # 1 ALERT
+// 1.
+// alert("First Name");
+// alert("Last Name");
+// alert("Email");
+// alert("Phone Number");
+// alert("Password  ");
+// 2.
+// alert("You're learning JavaScript!");
+// 3.
+// var user = prompt("Entry Your Name")
+// alert("Hope Your Are Fine");
+
+// =============================================================================================
+
+// Chapter 2 (Variables for string)
+// 1.
+// firstName = prompt("Enter Your Name");
+// fatherName = prompt("Enter Your Father Name");
+// console.log(firstName + " " + fatherName);
+// 2.
+// var teamName = "Pakistan";
+// alert("teamName");
+//3.
+// var a = "hello world";
+// console.log(a);
+// 4.
+// var bestMan = "Charlie";
+// var goodMan = "Jeo";
+// alert(bestMan + " " + goodMan);
+// =============================================================================================
+
+// Chapter 3 (Variables for numbers)
+//1.
+// var caseQty = ;
+//2.
+// var caseQty = "";
+// alert(caseQty);
+//3.
+// var operation = "9.6";
+// var a = Math.ceil(operation)
+// console.log(a);
+//4.
+// var b = 1 + 1;
+// console.log(b);
+//5.
+// var item = 500;
+// var taxCharges = 17;
+// var orderTotal = item + taxCharges;
+// console.log(orderTotal);
+// 6.
+// var add1 = 768;
+// var add2 = 786;
+// var addition = add1 + add2;
+// console.log(addition);
+// =============================================================================================
+
+// Chapter 4 (Variable names Legal and Illegal)
+// // 1.
+// var productCost = 3.45;
+// console.log(productCost);
+// 2.
+// var Nameofband;
+// var nameOfBand;
+//3.
+// var  userResponseTime = 101;
+//4.
+// var bestMan = "hammad";
+// var goodMan = "iqran";
+//5.
+// //==== legal====
+// var userResponseTime ;
+// var abc ;
+// var xyz123 ; 
+
+// //===illegal====
+// var abc@
+// var mnp#@#
+// var !a@b#c
+// =============================================================================================
+
+// Chapter 5 (Math Expression I)
+// 1.
+// var a = 10;
+// var b = 10;
+// var c = a % b;
+// alert(c);
+// 2.
+// var num = 20 % 6;
+// console.log(num);
+// 3.
+// var largeNum = 0.5 * 2000;
+// console.log(largeNum);
+// 4.
+// var variable1 = 1;
+// var variable2 = 1;
+// var variable = 1 - 1;
+// console.log(variable);
+// // 5.
+// var val1 = 500;
+// var val2 = 10;
+// var remainder = val1 % val2;
+// console.log(remainder)
+// 6.
+// alert (2*10)
+// =============================================================================================
+
+//Chapter 6 (Math Expression II)
+// 1.
+// var b = 99;
+// b++;
+// console.log(b);
+// 2.
+//  var m = 500;
+//  --m;
+//  console.log(m);
+// 3.
+//  var t = 50;
+//  var p = t++;
+//  console.log(p);
+// 4.
+//  var o = 50;
+//  var z = --o;
+// console.log(z);
+// 5.
+// var num0 = 9;
+// var num1 = num0++;
+// console.log(num1)
+// // 6.
+// var itemPrice = 98;
+// itemPrice++;
+// alert("New Price: " + itemPrice);
+// =============================================================================================
+
+//Chapter 7 (Math Expression III)
+// 1.
+// var calculatedNum = 2 + (2 * 6);
+// console.log(calculatedNum);
+// 2.
+// var calculatedNum2 = (2 + 2) * 6;
+// console.log(calculatedNum2);
+// 3.
+// var calculatedNum3 = (2 + 2) * (4 + 2);
+// console.log(calculatedNum3);
+// 4.
+// var calculatedNum4 = ((2 + 2) * 4) + 2;
+// console.log(calculatedNum4);
+// 5.
+// var cost = (2 + 2) * (4 + 10);
+// console.log(cost);
+// 6.
+// var units = 2 + (2 * (4 + 10));
+// console.log(units);
+// 7.
+// var pressure = (4 / 2) * 4;
+// console.log(pressure);
+// =============================================================================================
+
+//Chapter 8 (Concatenating TextStrings)
+// 1.
+// var number = "2" + "2";
+// console.log(number);
+// 2.
+// var message = ("Hello," + "Abu Bakar");
+// alert(message);
+// 3.
+// alert("33" + 3);
+// 4.
+// alert("Pakistan " + "Zindabad");
+// 5.
+// var result = "Hello" + 123;
+// console.log(result);
+// 6.
+// var variable1 = "Muhammad ";
+// var variable2 = "Iqran";
+// var result2 = variable1 + variable2;
+// alert(result2);
+// =============================================================================================
+
+// Chapter 9 (Prompts)
+//  var firstName = prompt("Enter Your First Name");
+// var contry = prompt("Enter Your Contry Name","Pakistan");
+// var yourName = prompt("Enter Your Name");
+// var DOB = prompt("Enter Your DOB","19-04-2002");
+// var message = "Enter Your Contry Name";
+// var contryName = "Pakistan";
+// var p = prompt(message , contryName);
+// alert(p);
+
+
+
+// Chapter 10 (if statments)
+// 1.
+// var city = "Karachi";
+// if(city == "Karachi"){
+//     console.log("The City Of Karachi");
+// }
+// 2.
+// var abc1 = 20;
+// var abc2 = 20;
+// if(abc1 === abc2){
+    // var pr = prompt("Enter Your pasword");
+// }
+// var zipCode = "10010";
+// if(zipCode === "10010"){
+//     alert("Karachi");
+// }else{
+//     alert("Please Write Correct City");
+// }
+// var xe = 900;
+// if(xe === 900){
+    //     xe = 300;
+// }
+// console.log(xe);
+
+//===========Chapter : 11================//
+
+// if(abc1 !== abc2){
+//     if(abc1 >= abc2){
+//         
+//     }
+//  }
